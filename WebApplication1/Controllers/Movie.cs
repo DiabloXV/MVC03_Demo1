e6a12bf3b6867160683af02a4d68d6dbf8690616
mlook <mlook@DESKTOP-MQTKHL3>
2024-09-12 04:34:37 +0300

    Add project files.

---
+++ WebApplication1/Controllers/Movie.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Movie : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(GetMovie), new { id = 10 });
+        }
+
+        public IActionResult GetMovie(int id)
+        {
+            return Content($"Movie with id = {id}");
+        }
+
+        
+
+    }
+}
